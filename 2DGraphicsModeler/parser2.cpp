362e9744d8613c6db2b72c4717d65965839dfbaf
hoonman <jh020812@gmail.com>
2022-07-21 07:44:36 -0700

    parser working for two shapes

--- 2DGraphicsModeler/parser2.cpp
+++ 2DGraphicsModeler/parser2.cpp
@@ -8,6 +8,11 @@
 parser2::parser2(){}
 
 // openFile Function
+//void parser2::openFile()
+//{
+//    std::ifstream infile("shapes.txt", std::ios::in);
+//}
+
 void parser2::readFile()
 {
     std::ifstream infile("shapes.txt", std::ios::in);
@@ -16,26 +21,38 @@ void parser2::readFile()
         std::cout << "file could not be opened." << std::endl;
         return;
     }
-    std::string color, penStyle, penCap, penJoin;
-    //getline(infile, text, '\n');
     getline(infile, text, ' ');
     getline(infile, item, '\n');
     shapeID = std::stoi(item);
     getline(infile, text, ' ');
     getline(infile, shape, '\n');
-    getline(infile, text, ' ');
-    getline(infile, item, ',');
-    point1 = std::stoi(item);
-    getline(infile, text, ' ');
-    getline(infile, item, ',');
-    point2 = std::stoi(item);
-    getline(infile, text, ' ');
-    getline(infile, item, ',');
-    point3 = std::stoi(item);
+//    if (shape == "Line")
+//    {
+//        readLine(infile);
+//        readPen(infile);
+//    }
+//    if (shape == "Polyline")
+//    {
+//        readPolyLine(infile);
+//        readPen(infile);
+//    }
+    readLine(infile);
+    readPen(infile);
+
     getline(infile, text, ' ');
     getline(infile, item, '\n');
-    point4 = std::stoi(item);
+    shapeID = std::stoi(item);
+    getline(infile, text, ' ');
+    getline(infile, shape, '\n');
 
+    readPolyLine(infile);
+    readPen(infile);
+    infile.close();
+}
+
+void parser2::readPen(std::ifstream& infile)
+{
+    std::string color, penStyle, penCap, penJoin;
     getline(infile, text, ' ');
     getline(infile, item, '\n');
     color = item;
@@ -52,10 +69,206 @@ void parser2::readFile()
     getline(infile, item, '\n');
     penJoin = item;
 
-    this->color = Qt::red;
-    this->penStyle = Qt::DashDotDotLine;
-    this->penCap = Qt::FlatCap;
-    this->penJoin = Qt::MiterJoin;
+    if (color == "white")
+    {
+        this->color = Qt::white;
+    }
+    if (color == "black")
+    {
+        this->color = Qt::black;
+    }
+    if (color == "red")
+    {
+        this->color = Qt::red;
+    }
+    if (color == "green")
+    {
+        this->color = Qt::green;
+    }
+    if (color == "blue")
+    {
+        this->color = Qt::blue;
+    }
+    if (color == "cyan")
+    {
+        this->color = Qt::cyan;
+    }
+    if (color == "magenta")
+    {
+        this->color = Qt::magenta;
+    }
+    if (color == "yellow")
+    {
+        this->color = Qt::yellow;
+    }
+    if (color == "gray")
+    {
+        this->color = Qt::gray;
+    }
 
-    infile.close();
+    if (penStyle == "NoPen")
+    {
+        this->penStyle = Qt::NoPen;
+    }
+    if (penStyle == "SolidLine")
+    {
+        this->penStyle = Qt::SolidLine;
+    }
+    if (penStyle == "DashLine")
+    {
+        this->penStyle = Qt::DashLine;
+    }
+    if (penStyle == "DotLine")
+    {
+        this->penStyle = Qt::DotLine;
+    }
+    if (penStyle == "NoPen")
+    {
+        this->penStyle = Qt::DashDotLine;
+    }
+    if (penStyle == "DashDotDotLine")
+    {
+        this->penStyle = Qt::DashDotDotLine;
+    }
+
+    if (penCap == "FlatCap")
+    {
+        this->penCap = Qt::FlatCap;
+    }
+    if (penCap == "SquareCap")
+    {
+        this->penCap = Qt::SquareCap;
+    }
+    if (penCap == "RoundCap")
+    {
+        this->penCap = Qt::RoundCap;
+    }
+
+    if (penJoin == "MiterJoin")
+    {
+        this->penJoin = Qt::MiterJoin;
+    }
+    if (penJoin == "BevelJoin")
+    {
+        this->penJoin = Qt::BevelJoin;
+    }
+    if (penJoin == "RoundJoin")
+    {
+        this->penJoin = Qt::RoundJoin;
+    }
+
+}
+
+void parser2::readLine(std::ifstream &infile)
+{
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    point1 = std::stoi(item);
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    point2 = std::stoi(item);
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    point3 = std::stoi(item);
+    getline(infile, text, ' ');
+    getline(infile, item, '\n');
+    point4 = std::stoi(item);
+}
+
+void parser2::readPolyLine(std::ifstream &infile)
+{
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, ',');
+    intVector.push_back(std::stoi(item));
+    getline(infile, text, ' ');
+    getline(infile, item, '\n');
+    intVector.push_back(std::stoi(item));
+
+
+    std::cout << intVector.size() << std::endl;
+}
+
+void parser2::readBrush(std::ifstream& infile)
+{
+    std::string brushColor, brushStyle;
+    getline(infile, text, ' ');
+    getline(infile, item, '\n');
+    brushColor = item;
+
+    if (brushColor == "white")
+    {
+        this->brushColor = Qt::white;
+    }
+    if (brushColor == "black")
+    {
+        this->brushColor = Qt::black;
+    }
+    if (brushColor == "red")
+    {
+        this->brushColor = Qt::red;
+    }
+    if (brushColor == "green")
+    {
+        this->brushColor = Qt::green;
+    }
+    if (brushColor == "blue")
+    {
+        this->brushColor = Qt::blue;
+    }
+    if (brushColor == "cyan")
+    {
+        this->brushColor = Qt::cyan;
+    }
+    if (brushColor == "magenta")
+    {
+        this->brushColor = Qt::magenta;
+    }
+    if (brushColor == "yellow")
+    {
+        this->brushColor = Qt::yellow;
+    }
+    if (brushColor == "gray")
+    {
+        this->brushColor = Qt::gray;
+    }
+
+    getline(infile, text, ' ');
+    getline(infile, item, '\n');
+    brushStyle = item;
+
+    if (brushStyle == "SolidPattern")
+    {
+        this->brushStyle = Qt::SolidPattern;
+    }
+    if (brushStyle == "HorPattern")
+    {
+        this->brushStyle = Qt::HorPattern;
+    }
+    if (brushStyle == "VerPattern")
+    {
+        this->brushStyle = Qt::VerPattern;
+    }
+    if (brushStyle == "NoBrush")
+    {
+        this->brushStyle = Qt::NoBrush;
+    }
+
 }
--- 2DGraphicsModeler/parser2.h
+++ 2DGraphicsModeler/parser2.h
@@ -15,6 +15,7 @@
 #include <QPainter>
 #include "Shape.h"
 #include "line.h"
+#include <QApplication>
 //using namespace std;
 
 class parser2
@@ -24,16 +25,23 @@ public:
     ~parser2() {}
     void readFile();
     int getShapeID() {return shapeID;}
+    void openFile();
     std::string getShape() {return shape;}
     int getPoint1() {return point1;}
     int getPoint2() {return point2;}
     int getPoint3() {return point3;}
     int getPoint4() {return point4;}
+    vector<int> getVPoints() {return intVector;}
     Qt::GlobalColor getColor() {return color;}
     int getWidth() {return width;}
     Qt::PenStyle getPenStyle() {return penStyle;}
     Qt::PenCapStyle getPenCap() {return penCap;}
     Qt::PenJoinStyle getPenJoinStyle() {return penJoin;}
+
+    void readPen(std::ifstream& infile);
+    void readLine(std::ifstream& infile);
+    void readPolyLine(std::ifstream& infile);
+    void readBrush(std::ifstream& infile);
 
 private:
     std::string item;
@@ -48,6 +56,9 @@ private:
     Qt::PenStyle penStyle;
     Qt::PenCapStyle penCap;
     Qt::PenJoinStyle penJoin;
+    Qt::BrushStyle brushStyle;
+    Qt::GlobalColor brushColor;
+
 };
 
 #endif // PARSER_H
